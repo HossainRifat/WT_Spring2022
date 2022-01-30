d559242841b8645c6486c410f4356c6da2aae985
HossainRifat <rh140025@gmail.com>
2022-01-30 11:35:06 +0600

    first Commit lab 1

---
+++ Task1/task1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Registration Form</h2>
+
+<form action="/action_page.php">
+<table>
+<tr>
+<td>First Name:</td>
+<td><input type="text" name="fname"></td>
+</tr>
+
+<tr>
+<td>Last name:</td>
+  <td><input type="text" name="lname"></td>
+</tr>
+
+<tr>
+<td>Age:</td>
+<td><input type="number" name="age"</td>
+</tr>
+
+<tr>
+  <td>Designation:</td>
+  <td><input type="radio" name = "language">Junior Programmer</td>
+  <td><input type="radio" name = "language">Senior Programmer</td>
+  <td><input type="radio" name = "language">Project Director</td>
+</tr>
+<tr>
+  <td>Preferred language:</td>
+  <td><input type="checkbox" name="language2">Java</td>
+  <td><input type="checkbox" name="language2">C++</td>
+  <td><input type="checkbox" name="language2">PHP</td>
+</tr>
+<tr>
+<td>Email:</td>
+<td><input type="email" name="email"></td>
+</tr>
+<tr>
+<td>Password:</td>
+<td><input type="password" name="pwd"></td>
+</tr>
+<tr>
+<td>Please Choose a file</td>
+<td><input type="file" name="myfile"></td>
+</tr>
+<tr> 
+<td><input type="submit" value="Submit">
+<input type="reset" value="Reset"></td>
+</tr>
+  
+  
+  
+  
+
