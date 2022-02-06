cfbff8b5e0b2712469eee910ede779527a15964d
HossainRifat <rh140025@gmail.com>
2022-02-06 11:37:11 +0600

    lab2

---
+++ Task2/control/results.php
@@ -0,0 +1,78 @@
+<?php
+    $fname="";
+    $lname ="";
+    //$password="";
+
+if(isset($_POST["Submit"])){
+
+    echo"<br><br><h2>Registration Confirmation</h2>";
+    $fname = $_POST["fname"];
+    if(empty($fname)){
+        echo"Enter a valid first name.<br>";
+    }
+    else{
+        echo"Your first name is ".$fname."<br>";
+    }
+    
+    $lname = $_POST["lname"];
+    if(empty($lname)){
+        echo"Enter a valid last name.<br>";
+    }
+    else{
+        echo"Your last name is ".$lname."<br>";
+    }
+
+    $age = $_POST["age"];
+    if(empty($age)){
+        echo"Enter valid age.<br>";
+    }
+    else{
+        echo"Your age is ".$age."<br>";
+    }
+
+    $password = $_POST["pwd"];
+    if(empty($password) || strlen($password)<7){
+        echo("Password must be more than 7 charecter.<br>");
+    }
+
+    if(!isset($_POST["language1"]) && !isset($_POST["language2"]) && !isset($_POST["language3"])){
+        echo"please select a radio button.<br>";
+    }
+    else{
+        if(isset($_POST["language1"])){
+            echo"you have selected junior programmer.<br>";
+        }
+        if(isset($_POST["language2"])){
+            echo"you have selected senior programmer.<br>";
+        }
+        if(isset($_POST["language3"])){
+            echo"you have selected project director.<br>";
+        }
+    }
+
+    if(! isset($_POST["language4"]) && !isset($_POST["language5"]) && !isset($_POST["language6"])){
+        echo"please select a checkbok.<br>";
+    }
+    else{
+
+        if(isset($_POST["language4"])){
+            echo"you have selected Java.<br>";
+        }
+        if(isset($_POST["language5"])){
+            echo"you have selected C++.<br>";
+        }
+        if(isset($_POST["language6"])){
+            echo"you have selected PHP.<br>";
+        }
+    }
+    $email = $_POST["email"];
+    if(empty($email) || !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix",$email)){
+        echo"invalid mail";
+    }
+
+
+
+
+}
+
+?>
---
+++ Task2/view/test.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Registration Form</h2>
+
+<form action="" method="post">
+<table>
+<tr>
+<td>First Name:</td>
+<td><input type="text" name="fname"></td>
+</tr>
+
+<tr>
+<td>Last name:</td>
+  <td><input type="text" name="lname"></td>
+</tr>
+
+<tr>
+<td>Age:</td>
+<td><input type="number" name="age"></td>
+</tr>
+
+<tr>
+  <td>Designation:</td>
+  <td><input type="radio" name = "language1">Junior Programmer
+  <input type="radio" name = "language2">Senior Programmer
+  <input type="radio" name = "language3">Project Director</td>
+</tr>
+<tr>
+  <td>Preferred language:</td>
+  <td><input type="checkbox" name="language4">Java
+  <input type="checkbox" name="language5">C++
+  <input type="checkbox" name="language6">PHP</td>
+</tr>
+<tr>
+<td>Email:</td>
+<td><input type="email" name="email"></td>
+</tr>
+<tr>
+<td>Password:</td>
+<td><input type="password" name="pwd"></td>
+</tr>
+<tr>
+<td>Please Choose a file</td>
+<td><input type="file" name="myfile"></td>
+</tr>
+<tr> 
+<td><input type="submit" name="Submit">
+<input type="reset" name="Reset"></td>
+</tr>
+</table>
+</body>
+</html>
+
+<?php
+include "../control/results.php";
+?>
+  
+
