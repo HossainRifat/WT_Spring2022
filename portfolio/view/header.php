17213ca7c22a40490fa0db0a8c41a9ab99289058
HossainRifat <rh140025@gmail.com>
2022-05-12 20:48:58 +0600

    d

---
+++ portfolio/view/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+</head>
+<body>
+    <div class="header">
+        <a href="" class="logo"><img src="../photo/site_logo.png" height="50" width="50"></a>
+        <a href="" class="name">Md. Rifat Hossain</a>
+        <div class="right-header">
+           <a href="" >About Me</a>
+           <a href="" >My Works</a>
+           <a href="" >Resume</a>
+           <a href="" >Contact</a>
+        </div>
+    </div>
+</body>
+</html>
